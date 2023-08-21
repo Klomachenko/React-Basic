dbbf244d3effebd35800a8bec3f8ae1f3c2a538f
Klomachenko <leekyumin0901@naver.com>
2023-08-21 20:17:42 +0900

    feat : destructuring 변수생성 문법, state 사용

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집'
+  // let [글제목1, b] = useState('여자 코트 추천')
+  // let [글제목2, c] = useState('용봉 라멘 맛집')
+  // let [글제목3, d] = useState('리액트 독학')
+  let [글제목, b] = useState(['여자 코트 추천', '용봉 라멘 맛집', '리액트 독학']) // 이게 조금 더 효율적이지 않을까?
 
   return (
     <div className="App">
       <div className="blakc-nav">
-        <h4>블로그임</h4>
+        <h4>ReactBlog</h4>
       </div>
-      <h4>{ post }</h4>
+      <div className='list'>
+        <h4>{ 글제목[0] }</h4>
+        <p>8월 21일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[1] }</h4>
+        <p>8월 21일 발행</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[2] }</h4>
+        <p>8월 21일 발행</p>
+      </div>
     </div>
   );
 }
