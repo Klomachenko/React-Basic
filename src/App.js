17b78069d4f559f7adb3a5200aa4a1e0d12049ce
Klomachenko <leekyumin0901@naver.com>
2023-08-22 01:13:13 +0900

    feat : 전개 연산자, array/object 동작원리

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { useState } from 'react';
 function App() {
 
   let post = '강남 우동 맛집'
-  let [글제목, b] = useState(['여자 코트 추천', '용봉 라멘 맛집', '리액트 독학'])
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '용봉 라멘 맛집', '리액트 독학'])
   let [따봉, 따봉변경] = useState(0)
 
   return (
@@ -13,6 +13,15 @@ function App() {
       <div className="blakc-nav">
         <h4>ReactBlog</h4>
       </div>
+
+      <button>가나다순 정렬</button>
+
+      <button onClick={()=>{
+         let copy = [...글제목]
+         copy[0] = '여자 코트 추천'
+         글제목변경(copy)
+         }}>글변경</button>
+
       <div className='list'>
         <h4>{ 글제목[0] } <span onClick={()=>{ 따봉변경(따봉+1) }}>👍</span> {따봉} </h4>
         <p>8월 21일 발행</p>
